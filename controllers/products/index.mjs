dc3368e3e133e4bc675e6ba8dab0ac15e85cee87
sarrahman <sarrahman.dev@gmail.com>
2022-04-08 05:18:07 +0800

    fix bug

--- controllers/products/index.mjs
+++ controllers/products/index.mjs
@@ -70,7 +70,9 @@ export const addProducts = async (req, res) => {
       }
     })
     .catch((error) => {
-      console.log(error);
+      res.status(500).json({
+        message: error.message,
+      });
     });
 };
 
